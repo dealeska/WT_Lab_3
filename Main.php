95749c8f1fdd0fe8eb9fe85c631aa1a45d5d110f
Alesya Deschenya <69769972+dealeska@users.noreply.github.com>
2021-03-27 20:06:29 +0300

    Add files via upload

---
+++ Main.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name = "viewport" content="width=device-width, initial-scale=1">
+    <meta name = "description" content="Спорт товары">
+    <title>SuperSport</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<section class = "section">
+    <div class = "container">
+        <div class="names" align="center">
+            <form method = "post" class="form" align="center">
+                <button class = "button">Получить все имеющиеся имена</button>
+                <br />
+            </form>
+        </div>
+
+        <div class="description" align="center">
+            <form method = "post" class="form" align="center">
+                <textarea class="information"></textarea>
+                <br />
+                <button class = "button" visability="hidden">Ок</button>
+                <br />
+                <br />
+            </form>
+        </div>
+
+
+    </div>
+</section>
+
+
+</body>
+</html>

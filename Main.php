4f508638f9754c9923606c76e32e6b320c401b52
Alesya Deschenya <69769972+dealeska@users.noreply.github.com>
2021-03-28 23:42:25 +0300

    Add files via upload
    Сделаны пост запросы. Осталось сделать 40%, вывод всех полей на экран.

--- Main.php
+++ Main.php
@@ -1,3 +1,31 @@
+<?php
+    function GetContentFromFile($fileName)
+    {
+        if (!file_exists($fileName))
+        {
+            $file = fopen($fileName, 'wb');
+            echo "noInformation";
+            fclose($file);
+        }
+        else
+        {
+            $file = fopen($fileName, 'rb');
+            while (($csv_data = fgetcsv($file, 100)) !== false)
+            {
+                echo "<div><a class = 'click' href=''>$csv_data[1]<br></a></div>";
+            }
+            fclose($file);
+        }
+    }
+
+    function PutContentToFile($fileName, $content)
+    {
+        $file = fopen($fileName, 'a');
+        fputcsv($file, $content, ',');
+        fclose($file);
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -5,16 +33,23 @@
     <meta name = "viewport" content="width=device-width, initial-scale=1">
     <meta name = "description" content="Спорт товары">
     <title>SuperSport</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="Style.css">
 </head>
 <body>
 
 <section class = "section">
     <div class = "container">
         <div class="names" align="center">
-            <form method = "post" class="form" align="center">
-                <button class = "button">Получить все имеющиеся имена</button>
+            <form method = "post" class="names" align="center">
+                <button class = "button" name ="getNames">Получить все имеющиеся имена</button>
+                <form method="post">
+                    <input class = "button" name ="addNames" formaction="AddContent.php" type="submit" value="Добавить товар">
+                </form>
                 <br />
+                <?
+                    if (isset($_POST['getNames']))
+                        GetContentFromFile('list.csv');
+                ?>
             </form>
         </div>
 
@@ -22,7 +57,7 @@
             <form method = "post" class="form" align="center">
                 <textarea class="information"></textarea>
                 <br />
-                <button class = "button" visability="hidden">Ок</button>
+                <button class = "button" name = "putName" visability="hidden">Ок</button>
                 <br />
                 <br />
             </form>
